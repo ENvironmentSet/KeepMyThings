c9731179095179aba2cf5c758cdcceb505f73286
ENvironmentSet <sbshw1@gmail.com>
2023-12-19 14:06:48 +0900

    fix model

--- server/KeepMyThings/streaming/models.py
+++ server/KeepMyThings/streaming/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+import uuid
 
 def videoPath(instance, _):
-  return f'{instance.user.id}/streams/{instance.id}'
+  return f'{instance.user.id}/streams/{instance.fileID}'
 
 class Stream(models.Model):
   user = models.ForeignKey(User, on_delete=models.CASCADE)
+  fileID = models.UUIDField(default=uuid.uuid4)
   video = models.FileField(upload_to=videoPath)
   croppedTime = models.DateTimeField('Date and time when video updated')
 
